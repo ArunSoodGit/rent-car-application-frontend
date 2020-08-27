72de50e8297c1479f95b38d10025efb38a7ca9db
Arun <arun.sood1998@gmail.com>
2020-08-27 16:59:27 +0200

    new relation with database

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {CarService} from '../services/car.service';
 import {Car} from '../models/Car';
+import {MatSort} from '@angular/material/sort';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-home',
@@ -9,17 +12,44 @@ import {Car} from '../models/Car';
 })
 export class HomeComponent implements OnInit {
 
+  displayedColumns: string[] = ['imię', 'nazwisko', 'email', 'nrTelefonu', 'pesel', 'nrPrawaJazdy', 'kraj', 'miasto', 'ulica', 'nrBudynku', 'nrLokalu', 'edit'];
+ 
+  dataSource;
+  customer;
+  searchKey: string;
 
-  carList: Car[] = [];
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   constructor(private carService: CarService) {
   }
 
-  ngOnInit(): void{
-    this.carService.getCars().subscribe(car =>
-      this.carList = car
+  ngOnInit(): void {
+    this.carService.getCars().subscribe(car => {
+        this.dataSource = new MatTableDataSource(car);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      }
     );
-
   }
 
+  onSearchClear() {
+    
+  }
+
+  applyFilter($event: KeyboardEvent) {
+    
+  }
+
+  onCreate() {
+    
+  }
+
+  onUpdate(element: any) {
+    
+  }
+
+  onDelete(element: any) {
+    
+  }
 }
